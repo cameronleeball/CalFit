fd1ac3e021b1efcc2f376c6748816e8d3e6cdf1e
Cameron Lee Ball <cameronleeball@gmail.com>
2017-06-16 20:07:37 -0400

    committing all of authentication changes

---
+++ app/controllers/authcontroller.js
@@ -0,0 +1,64 @@
+var passport = require('passport');
+var express = require("express");
+var router = express.Router();
+var Sequelize = require("sequelize");
+var db = require('../models/index.js');
+
+router.get('/',
+  function (req, res) {
+    res.render('index' /*, { user: req.db.user }*/);
+  });
+
+router.get('/sign-up',
+  function (req, res) {
+    res.render('sign-up', 
+    { layout: 'sign-up' });
+  });
+
+router.get('/login',
+  function (req, res) {
+    res.render('login', 
+    { layout: 'login' });
+  });
+
+router.post('/login',
+  passport.authenticate('local-login', 
+  { failureRedirect: '/login' }),
+  function (req, res) {
+    console.log("butts");
+    res.redirect('/');
+  });
+
+router.get('/logout',
+  function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+router.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function (req, res) {
+    // res.render('profile', { user: req.db.user });
+  });
+
+router.post('/sign-up', 
+  passport.authenticate('local-sign-up', {  
+    successRedirect: '/profile',
+    failureRedirect: '/sign-up'
+  }));
+
+router.put("/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  User.update({
+    devoured: req.body.devoured
+  }, condition, function () {
+    res.redirect("/");
+  });
+});
+
+
+
+module.exports = router;
--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 
 
 var express = require('express'),
-    Handlebars = require ('express-handlebars'),
+    Handlebars = require('express-handlebars'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
-    crypto = require('crypto'),
+    env = require('dotenv').load(),
     flash = require('connect-flash'),
     session = require('express-session'),
     bodyParser = require('body-parser');
@@ -14,58 +14,11 @@ var PORT = process.env.PORT || 8080;
 
 var MemoryStore = require('session-memory-store')(session);
 
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-
-passport.use(new LocalStrategy({
-    usernameField: 'uname',
-    passwordField: 'psw'
-    },
-    function (username, password, cb) {
-        User.findByUsername(username, function (err, user) {
-            if (err) { return cb(err); }
-            if (!user) { return cb(null, false); }
-            if (user.password != password) { return cb(null, false); }
-            return cb(null, user);
-        });
-    }));
-
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-
-
-
-
-passport.serializeUser(function (user, cb) {
-    cb(null, user.id);
-});
-
-passport.deserializeUser(function (id, cb) {
-    db.users.findById(id, function (err, user) {
-        if (err) { return cb(err); }
-        cb(null, user);
-    });
-});
-
-
-// Create a new Express application.
 var app = express();
 
 app.engine('handlebars', Handlebars({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
 app.use(require('cookie-parser')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -86,16 +39,15 @@ app.use(session({
 
 }));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static("./public"));
+app.use(express.static(__dirname + "/public"));
 
-var routes = require('./controllers/calfit_controller.js');
+var routes = require('./controllers/authcontroller.js');
+
+require('./lib/passport/passport.js')(passport, db.User);
 
 app.use('/', routes);
 
